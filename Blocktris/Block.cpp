cd96999c62f6be9b233577d68def79d101e8d7d2
Sterling <sterlingangrish@gmail.com>
2026-05-01 12:16:12 -0700

    Preliminary code for Andy Bossfight

--- Blocktris/Block.cpp
+++ Blocktris/Block.cpp
@@ -1072,3 +1072,41 @@ void OBlock::rotateBlockCounter()
 {
 	return;
 }
+
+void ABlock::constructBlock()
+{
+	int newSize[4][4] = {
+		{ 1, 0, 0, 0 },
+		{ 0, 0, 0, 0 },
+		{ 0, 0, 0, 0 },
+		{ 0, 0, 0, 0 }
+	};
+	setSize(newSize);
+	rotation = 0;
+	color = 9;
+
+	int spawnCol = COLUMNS / 2 - 2;
+	for (int r = 0; r < 4; ++r)
+	{
+		for (int c = 0; c < 4; ++c)
+		{
+			if (size[r][c] == 1)
+			{
+				currentPos.push_back({ spawnCol + c, r });
+				currentPos.push_back({ spawnCol + c, r });
+				currentPos.push_back({ spawnCol + c, r });
+				currentPos.push_back({ spawnCol + c, r });
+			}
+		}
+	}
+}
+
+void ABlock::rotateBlock()
+{
+	return;
+}
+
+void ABlock::rotateBlockCounter()
+{
+	return;
+}
