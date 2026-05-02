04f6617e1a8c3010055bb6f08f5f02adb5d89af1
SogyCracker <kadynn32@gmail.com>
2026-05-01 23:51:23 -0700

    Refactor Character class into CharAnimation class

--- Blocktris/Character.cpp
+++ Blocktris/Character.cpp
@@ -1,32 +1,134 @@
-#include "Character.h"
+#include "CharAnimation.h"
 
-Character::Character()
+CharAnimation::CharAnimation() {}
+
+bool CharAnimation::loadSheet(const std::string& filename)
 {
-    // sprite will be created AFTER texture loads
+    return sheet.loadFromFile(filename);
 }
 
-bool Character::load(const std::string& filename)
+void CharAnimation::start(sf::Sprite& targetSprite, int animationNum)
 {
-    if (!texture.loadFromFile(filename))
-        return false;
 
-    // NOW we can construct the sprite legally
-    sprite.emplace(texture);
+    if (animationNum == 1)
+    {
+        playing = true;
+        currentFrame = 0;
+        frameTimer = 0;
 
-    return true;
+        targetSprite.setTexture(sheet);
+
+        // START ON FRAME 0
+        // upset andy
+        targetSprite.setTextureRect(sf::IntRect(
+            sf::Vector2i(0, 0),
+            sf::Vector2i(frameWidth, frameHeight)
+        ));
+    }
+    else if (animationNum == 2)
+    {
+        playing2 = true;
+        currentFrame = 0;
+        frameTimer = 0;
+
+        targetSprite.setTexture(sheet);
+
+        // START ON FRAME 0
+        // angry andy
+        sf::IntRect rect(
+            sf::Vector2i(4 * frameWidth2, 8 * frameHeight2),
+            sf::Vector2i(frameWidth2, frameHeight2)
+        );
+    }
 }
 
-void Character::setPosition(const sf::Vector2f& pos)
+bool CharAnimation::isPlaying(int animationNum) const
 {
-    if (sprite.has_value())
-        sprite->setPosition(pos);
-    
-    //2x andy
-    sprite->setScale(sf::Vector2f(2.f, 2.f));
+    if (animationNum == 1)
+    {
+        return playing;
+    }
+    if (animationNum == 2)
+    {
+        return playing2;
+    }
 }
 
-void Character::draw(sf::RenderWindow& window) const
+void CharAnimation::update(sf::Sprite& targetSprite, int animationNum)
 {
-    if (sprite.has_value())
-        window.draw(*sprite);
+    if(animationNum == 1)
+    {
+        if (!playing)
+            return;
+
+        ++frameTimer;
+
+        if (frameTimer >= 5) // animation speed
+        {
+            frameTimer = 0;
+            ++currentFrame;
+
+            if (currentFrame >= totalFrames)
+            {
+                playing = false;
+                return;
+            }
+
+            // compute row/column
+            int col = currentFrame % 3;
+            int row = currentFrame / 3;
+
+            sf::IntRect rect(
+                sf::Vector2i(col * frameWidth, row * frameHeight),
+                sf::Vector2i(frameWidth, frameHeight)
+            );
+
+            targetSprite.setTextureRect(rect);
+        }
+    }
+    else if (animationNum == 2)
+    {
+        if (!playing2)
+            return;
+
+        ++frameTimer;
+
+        if (frameTimer >= 10) // animation speed
+        {
+            frameTimer = 0;
+            ++currentFrame;
+
+            if (currentFrame >= totalFrames2)
+            {
+                playing2 = false;
+                return;
+            }
+
+            int col = currentFrame % 4;
+            int row = currentFrame / 4;
+
+            sf::IntRect rect(
+                sf::Vector2i(col * frameWidth2, row * frameHeight2),
+                sf::Vector2i(frameWidth2, frameHeight2)
+            );
+
+            targetSprite.setTextureRect(rect);
+        }
+    }
+}
+
+void CharAnimation::stop(int animationNum)
+{
+    if (animationNum == 1)
+    {
+        playing = false;
+        currentFrame = 0;
+        frameTimer = 0;
+    }
+    else if (animationNum == 2)
+    {
+        playing2 = false;
+        currentFrame = 0;
+        frameTimer = 0;
+    }
 }
