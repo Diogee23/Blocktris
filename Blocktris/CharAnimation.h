0e47fe1c66d1777e4ee19edc7ba0f856c66a3fd5
SogyCracker <kadynn32@gmail.com>
2026-05-01 23:50:57 -0700

    Refactor CharAnimation methods and variables

--- Blocktris/CharAnimation.h
+++ Blocktris/CharAnimation.h
@@ -7,36 +7,24 @@ public:
     CharAnimation();
     bool loadSheet(const std::string& filename);
 
-    void start(sf::Sprite& targetSprite);
-    void update(sf::Sprite& targetSprite);
-    bool isPlaying() const;
-
+    void start(sf::Sprite& targetSprite, int animationNum);
+    void update(sf::Sprite& targetSprite, int animationNum);
+    bool isPlaying(int animationNum) const;
+    void stop(int animationNum);
 
 private:
-    sf::Texture sheet;
+    
     int frameWidth = 198;
     int frameHeight = 198;
     int totalFrames = 10;
+    bool playing = false;
 
+    sf::Texture sheet;
     int currentFrame = 0;
     int frameTimer = 0;
-    bool playing = false; //
-
-    sf::Texture angered_sheet;
-    int frameWidth2 = 1188;
-    int frameHeight2 = 1188;
+    
+    int frameWidth2 = 297;
+    int frameHeight2 = 198;
     int totalFrames2 = 27;
-
-    int currentFrame2 = 0;
-    int frameTimer2 = 0;
-    bool playing2 = false; //wrath_of_andy
-
-	// sf::Texture angered_sheet;
-	// int frameWidth3 = 990;
-	// int frameHeight3 = 1188;
-	// int totalFrames3 = 27;
-
-	// int currentFrame3 = 0;
-	// int frameTimer3 = 0;
-	// bool teehee = false;
+    bool playing2 = false;
 };
